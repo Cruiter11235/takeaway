3bf1f3b7691ae7779fd30c27acdc2483a353627a
cruiter <cruiter11235@outlook.com>
2023-10-29 17:05:27 +0800

    1.0

---
+++ src/test/java/com/zjj/mybatisplusdemo/MybatisPlusDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zjj.mybatisplusdemo;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
